bc218ee914a7231a04ec30b21e5737262b60517e
Artem <dumzday777@gmail.com>
2023-09-10 15:31:58 +0300

    2.0

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
 const sourcemaps = require('gulp-sourcemaps')
 const size = require('gulp-size')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const htmlmin = require('gulp-htmlmin')
+const newer = require('gulp-newer')
+const browsersync = require('browser-sync').create()
+const gulppug = require('gulp-pug')
 
 
 
@@ -37,42 +43,75 @@ function clean() {
     return del(['dist/*', '!dist/img'])
   }
 
-function styles() {
-    return gulp.src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
-    .pipe(rename({
-      basename: 'style',
-      suffix: '.min'
-    }))
-    .pipe(size({
-      showFiles:true
-    }))
-    .pipe(gulp.dest(paths.styles.dest))
-
-  }
-function scripts() {
-  return gulp.src(paths.scripts.src, {sourcemaps:true})
-
-  .pipe(babel())
-  .pipe(uglify())
-  .pipe(concat('main.min.js'))
-
+function html() {
+  return gulp.src(paths.html.src)
+  .pipe(gulppug())
+  .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(size({
     showFiles:true
   }))
+  .pipe(gulp.dest(paths.html.dest))
+  .pipe(browsersync.stream())
+
+}
+
+function styles() {
+  return gulp.src(paths.styles.src)
+  .pipe(sourcemaps.init()) //щоб в браузері показувало в якій стрічці коду
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    cascade: false
+  }))
+  .pipe(cleanCSS({level:2}))
+  .pipe(rename({
+    basename: 'style',
+    suffix: '.min'
+  }))
+  .pipe(sourcemaps.write('.'))
+  .pipe(size())
+  .pipe(gulp.dest(paths.styles.dest))
+  .pipe(browsersync.stream())
+
+}
+function scripts() {
+  return gulp.src(paths.scripts.src, {sourcemaps:true})
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(uglify())
+  .pipe(concat('main.min.js'))
+  .pipe(sourcemaps.write('.'))
+  .pipe(size())
   .pipe(gulp.dest(paths.scripts.dest))
+  .pipe(browsersync.stream())
 
 } 
+function img() {
+  return gulp.src(paths.images.src)
+  .pipe(newer(paths.images.dest))
+  .pipe(imagemin({
+    progressive: true
+  }))
+  .pipe(size())
+  .pipe(gulp.dest(paths.images.dest))
+}
+
 
 function watch() {
-
+  browsersync.init({
+    server: {
+        baseDir: "./dist"
+    }
+  })
+     gulp.watch(paths.html.dest).on('change', browsersync.reload)
+    gulp.watch(paths.html.src, html)
     gulp.watch(paths.scripts.src, scripts)
     gulp.watch(paths.styles.src, styles)
 
   }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
 
 
 exports.clean = clean
